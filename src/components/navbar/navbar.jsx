350c0beb912c7c415b9fcc39b7da5978b8f0c403
davil5638 <davilimap11@gmail.com>
2021-11-08 15:42:20 -0300

    (0001) Primeiro Entregável
    Foi feito o header, os cards, e a responsividade.

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default Navbar() {
+  return (
+    <>
+      <nav id="menu">
+        <div class="logo">
+            <a href="">Logo </a> 
+        </div>
+        <div class="nome">
+            <h2>Poke.search</h2>
+        </div>
+      </nav>
+    </>
+  )
+}
