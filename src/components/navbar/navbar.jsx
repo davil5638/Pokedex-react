a1ba6e9335336fd84bd03da427812fa0c8c628be
davil5638 <davilimap11@gmail.com>
2021-11-09 17:26:31 -0300

    update
    update

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import "./navbar.css";
 import imagem from "../../assets/images/logo.png";
+import imagem2 from "../../assets/images/fundo2.jpg";
 
 export default function Navbar() {
   return (
     <>
       <nav id="menu">
+        <img class="teste" src={imagem2} />
         <div class="logo">
           <a href="#">
-            <img src={imagem} />
+            <img class="fundo" src={imagem} />
           </a>
         </div>
       </nav>
     </>
   );
 }
-[]
+[];
